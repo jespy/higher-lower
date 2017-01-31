83e7a32c2bb0250fd9595022c2f5b4906ce70dd4
Chris Jespersen <cwjespersen@gmail.com>
2017-01-31 00:19:02 -0500

    finished tut

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ var userGuessLog = [];
 var attempts = 0;
 var maxGuesses = 10;
 
+function gameEnded() {
+document.getElementById('newGameButton').style.display='inline';
+	document.getElementById('easyBtn').style.display='none';
+	document.getElementById('hardBtn').style.display='none';
+	document.getElementById('inputBox').setAttribute('readonly', 'readonly');
+}
+
 function easyMode() {
 	maxGuesses = 10;
 	document.getElementById('easyBtn').className = 'activeButton';
@@ -39,20 +46,28 @@ function compareGuess() {
 	if(userGuessLog.length < maxGuesses){
 		if(userGuess > computerGuess) {
 			document.getElementById('textOutput').innerHTML = 'Your guess is too high';
-			document.getElementById('inputBox').value="";
+			document.getElementById('inputBox').value= "";
 		} else if (userGuess < computerGuess) {
 			document.getElementById('textOutput').innerHTML = 'Your guess is too low';
-			document.getElementById('inputBox').value="";
+			document.getElementById('inputBox').value = "";
 		} else {
-			document.getElementById('textOutput').innerHTML = 'Correct!';
+			document.getElementById('textOutput').innerHTML = 'Correct! you got it in ' + attempts + ' attempts';
+			document.getElementById('container').style.backgroundColor = 'green';
+			gameEnded();
 		}
 	} else {
 		 if(userGuess > computerGuess) {
-	 		document.getElementById('textOutput').innerHTML = 'You Lose!';
+	 		document.getElementById('textOutput').innerHTML = 'You Lose!' + '<br> the number was ' + computerGuess;
+	 		document.getElementById('container').style.backgroundColor = '#e82c4e';
+	 		gameEnded();
  		} else if (userGuess < computerGuess) {
-			document.getElementById('textOutput').innerHTML = 'You Lose!';
+			document.getElementById('textOutput').innerHTML = 'You Lose!' + '<br> the number was ' + computerGuess;
+			document.getElementById('container').style.backgroundColor = '#e82c4e';
+			gameEnded();
  		} else {
-	 		document.getElementById('textOutput').innerHTML = 'Correct';
+	 		document.getElementById('textOutput').innerHTML = 'Correct! you got it in ' + attempts + ' attempts';
+	 		document.getElementById('container').style.backgroundColor = 'green';
+	 		gameEnded();
 
  	}
 
